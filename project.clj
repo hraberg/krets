9c80754e71e5632159d8f2c96e660df7215c686d
Håkan Råberg <hraberg@yahoo.se>
2015-01-10 18:12:19 +0000

    clojure-1.7.0-alpha5

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/hraberg/krets"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-alpha4"]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
                  [org.jfree/jfreechart "1.0.19"]
                  [net.mikera/core.matrix "0.32.1"]
                  [net.mikera/vectorz-clj "0.28.0"]
