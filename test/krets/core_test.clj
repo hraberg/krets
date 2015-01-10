c756dd9b2dcd290eacd5da29867bd05d383c54bf
Håkan Råberg <hraberg@yahoo.se>
2015-01-10 02:25:15 +0000

    Test using both vectorz and clatrix

--- test/krets/core_test.clj
+++ test/krets/core_test.clj
@@ -1,20 +1,24 @@
 (ns krets.core-test
   (:use [krets.core]
         [clojure.test])
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [clojure.core.matrix.implementations :refer [*matrix-implementation*]]))
 
 (deftest sanity-check
   (with-redefs [plot (fn [xt yt & _]
                        (println "< plotting " xt yt ">"))]
-    (doseq [f (file-seq (io/file "test/krets/"))
+    (doseq [impl [:vectorz :clatrix]
+            f (file-seq (io/file "test/krets/"))
             :when (re-find #"\.cir$" (str f))]
-      (let [fail (volatile! nil)
-            out (with-out-str
-                  (try
-                    (process-file f)
-                    (is true)
-                    (catch Exception e
-                      (vreset! fail e))))]
-        (when-let [e @fail]
-          (println out)
-          (throw e))))))
+      (binding [*matrix-implementation* impl]
+        (let [fail (volatile! nil)
+              out (with-out-str
+                    (try
+                      (process-file f)
+                      (is true)
+                      (catch Exception e
+                        (vreset! fail e))))]
+          (when-let [e @fail]
+            (println impl)
+            (println out)
+            (throw e)))))))
